2e0f88f90e7afdc0aef77dc700e0a33459e8faa9
thrustmaster <supersaiyanmode.rox@gmail.com>
2013-01-22 00:41:30 +0530

    A better looking sample, no really!

--- main.cpp
+++ main.cpp
@@ -12,8 +12,19 @@
 #include "Util/Thread/ThreadLogger.h"
 #include "Util/Thread/Thread.h"
 
+#include "Util/Regex/RegexMatcher.h"
+
 class MyCMSServer: public CMSServer {
-	
+	Thread<MyCMSServer, int>* thread;
+public:
+    MyCMSServer(int p): CMSServer(p){
+        thread = Thread<MyCMSServer, int>::createThread(this, &MyCMSServer::serve);
+        thread->start(0);
+    }
+    
+    void serve(int){
+        CMSServer::serve();
+    }
 };
 
 class MyQueueReceiver: public QueueReceiver {
@@ -21,6 +32,10 @@ public:
     MyQueueReceiver(CMSServerConnection& conn, const std::string& dest):
             QueueReceiver(conn, dest){
         tlog("QueueReceiver initialised!");
+    }
+    
+    void onMessage(GenericCMSMessage& msg){
+        tlog("INCOMING MESSAGE:\n"<<msg.str());
     }
 };
 
@@ -30,10 +45,22 @@ public:
             QueueSender(conn, dest){
         tlog("QueueSender initialised!");
     }
+    
+    void send(std::string m){
+        QueueSender::send(m);
+    }
 };
 
 int main(){
+    MyCMSServer server(8097);
+    ThreadBase::sleep(100);
+    CMSServerConnection* conn = CMSServerConnection::createCMSServerConnection(
+                    "127.0.0.1", 8097);
+    MyQueueSender sender1(*conn, "ABC");
+    MyQueueReceiver recr1(*conn, "ABC");
+    sender1.send("BLAH!");
     
+    ThreadBase::sleep(10000);
     return 0;
 }
 #endif
